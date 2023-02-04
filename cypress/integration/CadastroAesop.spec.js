2afe664eeb04aa034881b63e4f720a213fb93c5d
Roni <roni.jimmylee@gmail.com>
2023-02-03 23:57:49 -0300

    update cenario aesop

--- cypress/integration/CadastroAesop.spec.js
+++ cypress/integration/CadastroAesop.spec.js
@@ -14,6 +14,8 @@ const campoDataNasc = ":nth-child(4) > :nth-child(1) > .MuiFormControl-root > .M
 const generoNaoEspec = "#root > div:nth-child(1) > div > div > div > form > div.MuiBox-root.natds83.natds73 > div:nth-child(3) > div:nth-child(2) > div.MuiFormGroup-root.natds77 > label:nth-child(3) > span.MuiButtonBase-root.MuiIconButton-root.natds112.MuiRadio-root.MuiRadio-colorPrimary.MuiIconButton-colorPrimary > span.MuiIconButton-label > input"
 const campoTelefone = ":nth-child(4) > :nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input"
 const btnAceptTerm = "#acceptedterms"
+const btnCriarConta = ".MuiButton-label"
+const perfilLogado = ".natds3 > :nth-child(3) > .MuiBox-root"
 
 describe('Testes Aesop', () => {
 
@@ -22,6 +24,8 @@ describe('Testes Aesop', () => {
         cy.preecherCadastro(PrimeiroNome, Sobrenome, email, campoSenha, campoRepitaSenha, campoCpf, campoDataNasc, campoTelefone)
         cy.get(generoNaoEspec).click();
         cy.get(btnAceptTerm).click();
+        cy.get(btnCriarConta).click();
+        cy.get(perfilLogado).click().should('be.visible');
     });
 
 });
