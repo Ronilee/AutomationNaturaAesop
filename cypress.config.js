0b1874b24e5495dad38eeed38be4fb743e4c2fc1
Roni <roni.jimmylee@gmail.com>
2023-02-09 13:13:44 -0300

    Atuallização de reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,15 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
 
-  integration: {
+  e2e: {
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+ });
